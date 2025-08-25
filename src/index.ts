4faf0e39ae886012f5df50cd0c0b2edd3ee9aafc
clinnyp <cphilathong1@gmail.com>
2025-08-25 23:03:44 +1200

    add gemini chat response endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,10 @@
 import { Hono } from 'hono'
 import { serve } from '@hono/node-server'
 import { handle } from 'hono/aws-lambda'
-import { db } from './shared/db/db'
-import { notes, users } from './shared/db/schema'
 import { logger } from 'hono/logger'
 import notesRouter from './lambdas/notes'
 import usersRouter from './lambdas/users'
+import { GoogleGenAI } from '@google/genai'
 
 
 const app = new Hono()
@@ -13,6 +12,27 @@ const app = new Hono()
 app.use('*', logger())
 app.route('/notes', notesRouter)
 app.route('/users', usersRouter)
+
+// generate chat response
+app.post('/generate_chat', async (c) => {
+  try {
+    const ai = new GoogleGenAI({
+      apiKey: process.env.GEMINI_API_KEY
+    })
+
+    const body = await c.req.json()
+    const { prompt } = body
+
+    const response = await ai.models.generateContent({
+      model: "gemini-2.5-flash",
+      contents: prompt
+    })
+
+    return c.json({ message: "Chat Generated Successfully", chat: response.text }, 200)
+  } catch (error) {
+    return c.json({ message: 'Error generating chat response', error }, 500)
+  }
+})
 
 // Local development server
 const port = 3000
